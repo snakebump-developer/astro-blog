191ae4446901d0533a5de40da06b80a7a4abdad3
Alessandro Sylar <lsn.lodigiani@gmail.com>
2025-11-25 00:10:40 +0100

    Commit iniziale

---
+++ src/constants.ts
@@ -0,0 +1,3 @@
+export const SITE_DESCRIPTION = "Tech People Blog - Your source for the latest in technology, programming, and software development.";
+export const HOMEPAGE_ARTICLE_LIMIT = 6;
+export const ARTICLES_PER_PAGE = 6;
